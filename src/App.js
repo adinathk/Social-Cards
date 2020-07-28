e1e52154507e5de6ac29da830c528c9d6e5f8a22
Adinath <55339116+adinathk@users.noreply.github.com>
2020-07-28 09:18:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './components/Card';
+import CardMain from './components/CardMain';
+
+function App() {
+  return (
+    <div className="Main">
+        <CardMain/>
+    </div>
+  );
+}
+
+export default App;
